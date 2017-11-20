d7bcf26d8439c03bbb6d25924c5255a151aeba03
Freibuis <freibuis@gmail.com>
2017-11-20 20:28:43 +1000

    not every one uses rails..tsk tsk

--- lib/simple_features/features.rb
+++ lib/simple_features/features.rb
@@ -2,7 +2,7 @@ module SimpleFeatures
   class Features
     attr_accessor :features
 
-    def initialize(features={})
+    def initialize(features = {})
 
       @features = features
       make_features
@@ -10,10 +10,9 @@ module SimpleFeatures
     end
 
     #returns Feature object
-    def self.load_config(config_file=nil)
+    def self.load_config(config_file = nil)
       if defined? 'Rails'
-        features = YAML.load_file(
-            Rails.root.join('config', 'features.yml'))[Rails.env]
+        features = YAML.load_file(Rails.root.join('config', 'features.yml'))[environment]
       else
         features = YAML.load_file(config_file)
       end
@@ -28,6 +27,14 @@ module SimpleFeatures
         define_singleton_method("#{feature}?") do
           feature_value
         end
+      end
+    end
+
+    def environment
+      if defined? 'Rails'
+        Rails.env
+      else
+        :production
       end
     end
   end
